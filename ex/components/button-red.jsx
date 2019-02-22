b2476f6bfa5917be56573f421742534fc2a4c26a
Felipe Rodrigues Michetti <frmichetti@gmail.com>
2019-02-22 17:58:37 -0300

    added sample html

--- ex/components/button-red.jsx
+++ ex/components/button-red.jsx
@@ -1,6 +1,4 @@
 import React from 'react';
 import { Button } from 'reactstrap';
 
-export default props => (
-    <Button color="danger">Danger!</Button>
-);
+export default props => (<Button color="danger">Danger!</Button>);
