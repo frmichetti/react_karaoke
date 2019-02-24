1ee9c70fb422c7fc343b07eefb43f937239957c2
Felipe Rodrigues Michetti <frmichetti@gmail.com>
2019-02-24 16:38:21 -0300

    fix jquery

--- public/js/player.js
+++ public/js/player.js
@@ -1,65 +1,68 @@
-console.clear();
-var _data;
-var currentLine = "";
+(function($){
 
-var promise = $.get('http://localhost:3000/lyrics/enter-sandman.lyric.json')
-promise.then(function(data){
-  _data = data;
+  console.clear();
+  var _data;
+  var currentLine = "";
 
-  function align() {
-    var a = $(".highlighted").height();
-    var c = $(".content").height();
-    var d = $(".highlighted").offset().top - $(".highlighted").parent().offset().top;
-    var e = d + (a / 2) - (c / 2);
-    $(".content").animate(
-      {scrollTop: e + "px"}, {easing: "swing", duration: 250}
-    );
-  }
+  var promise = fetch('http://localhost:3000/lyrics/enter-sandman.lyric.json')
+  promise.then(function(response){
+    response.json().then(function (data) {
+      _data = data;
 
-  var lyricHeight = $(".lyrics").height();
-  $(window).on("resize", function () {
-    if ($(".lyrics").height() != lyricHeight) { //Either width changes so that a line may take up or use less vertical space or the window height changes, 2 in 1
-      lyricHeight = $(".lyrics").height();
-      align();
-    }
-  });
+      function align() {
+        var a = $(".highlighted").height();
+        var c = $(".content").height();
+        var d = $(".highlighted").offset().top - $(".highlighted").parent().offset().top;
+        var e = d + (a / 2) - (c / 2);
+        $(".content").animate(
+          {scrollTop: e + "px"}, {easing: "swing", duration: 250}
+        );
+      }
 
-  $(document).ready(function () {
-    $("video").on('timeupdate', function (e) {
-      var time = this.currentTime * 1000;
-      var past = _data["lyrics"].filter(function (item) {
-        return item.time < time;
+      var lyricHeight = $(".lyrics").height();
+      $(window).on("resize", function () {
+        if ($(".lyrics").height() != lyricHeight) { //Either width changes so that a line may take up or use less vertical space or the window height changes, 2 in 1
+          lyricHeight = $(".lyrics").height();
+          align();
+        }
       });
-      if (_data["lyrics"][past.length] != currentLine) {
-        currentLine = _data["lyrics"][past.length];
-        $(".lyrics div").removeClass("highlighted");
-        $(`.lyrics div:nth-child(${past.length})`).addClass("highlighted"); //Text might take up more lines, do before realigning
+
+      $(document).ready(function () {
+        $("video").on('timeupdate', function (e) {
+          var time = this.currentTime * 1000;
+          var past = _data["lyrics"].filter(function (item) {
+            return item.time < time;
+          });
+          if (_data["lyrics"][past.length] != currentLine) {
+            currentLine = _data["lyrics"][past.length];
+            $(".lyrics div").removeClass("highlighted");
+            $(`.lyrics div:nth-child(${past.length})`).addClass("highlighted"); //Text might take up more lines, do before realigning
+            align();
+          }
+        });
+      });
+
+      generate();
+
+      function generate() {
+        var html = "";
+        for (var i = 0; i < _data["lyrics"].length; i++) {
+          html += "<div";
+          if (i == 0) {
+            html += ` class="highlighted"`;
+            currentLine = 0;
+          }
+          if (_data["lyrics"][i]["note"]) {
+            html += ` note="${_data["lyrics"][i]["note"]}"`;
+          }
+          html += ">";
+          html += _data["lyrics"][i]["line"] == "" ? "•" : _data["lyrics"][i]["line"];
+          html += "</div>"
+        }
+        $(".lyrics").html(html);
         align();
       }
-    });
-  });
-
-  generate();
-
-  function generate() {
-    var html = "";
-    for (var i = 0; i < _data["lyrics"].length; i++) {
-      html += "<div";
-      if (i == 0) {
-        html += ` class="highlighted"`;
-        currentLine = 0;
-      }
-      if (_data["lyrics"][i]["note"]) {
-        html += ` note="${_data["lyrics"][i]["note"]}"`;
-      }
-      html += ">";
-      html += _data["lyrics"][i]["line"] == "" ? "•" : _data["lyrics"][i]["line"];
-      html += "</div>"
-    }
-    $(".lyrics").html(html);
-    align();
-  }
-
-})
-
+    })
+  })
+}(jQuery));
 
